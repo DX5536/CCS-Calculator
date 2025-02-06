97cd67281d56fa39ae805c71174ec33c42364e1f
Hai Lam Dang <hai.dang@ue-germany.de>
2025-02-06 20:40:57 +0100

    [ADD] Some basic UI and placeholder script

---
+++ Assets/Scripts/Spiral_Calculation.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class Spiral_Calculation: MonoBehaviour
+{
+    [Header("80g")]
+    [SerializeField]
+    private PriceFactor_SO[] black_80_pagePrices;
+    [SerializeField]
+    private PriceFactor_SO[] color_80_pagePrices;
+    [Header("120g")]
+    [SerializeField]
+    private PriceFactor_SO[] black_120_pagePrices;
+    [SerializeField]
+    private PriceFactor_SO[] color_120_pagePrices;
+
+    [Header("Binding Art")]
+    [SerializeField]
+    private PriceFactor_SO[] bindingArtPrices;
+
+    //Bool value to check if it's 80g, if not it means it's 120g
+    [SerializeField]
+    private bool is80g;
+    //Bool value to check if it's colored print, if not it means it's s/w print
+    [SerializeField]
+    private bool isColor;
+    //Bool value to check if it is printed here, if not it means paper is bought by customer
+    [SerializeField]
+    private bool isPrintedHere;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void UpdateValue_Spiral()
+    {
+
+    }
+
+    //public method to access from button
+    public void Spiral_A4_PrintHere()
+    {
+
+    }
+}
